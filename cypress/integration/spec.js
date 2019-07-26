f88a1ef982487d1178f2f0a32bb84ec74497ea06
Kyle Burnett <kburnett@proknowsystems.com>
2019-07-26 08:41:57 -0400

    Initial commit of test case

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,14 @@
 describe('page', () => {
-  it('works', () => {})
+    it('should', () => {
+        cy.visit('https://docs.cypress.io');
+        cy.get('nav#main-nav').should(($nav) => {
+            var node = $nav[0];
+            var guides = node.querySelector('ul li:first-child a');
+            var $guides = Cypress.$(guides);
+
+            // Both of these assertions work in Cypress v3.3.2 and fail in Cypress v3.4.0
+            expect(guides).to.be.visible.and.have.class('active');
+            expect($guides).to.be.visible.and.have.class('active');
+        });
+    });
 })
